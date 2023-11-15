68842fca5a68592db07160afbfc81c0219c96a04
Tiago Neto <tiagocastroneto@yahoo.com>
2023-11-15 14:23:54 +0000

    challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "japanese"}
+carlos = {name: "Carlos Place", address: "Matosinhos", category: "french"}
+vasco = {name: "Vasco Place", address: "São Pedro da Cova", category: "belgian"}
+tiago = {name: "Tiago Trattoria", address: "Santo Tirso", category: "italian"}
+
+[dishoom, pizza_east, carlos, vasco, tiago].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
